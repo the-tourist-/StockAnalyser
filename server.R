7c4dc7e978cf922c2aa8b949c8022742feea1c66
Graeme <graemesmith70@gmail.com>
2015-03-22 22:27:21 +0100

    Final Version

--- server.R
+++ server.R
@@ -1,7 +1,87 @@
 library(shiny)
+library(lubridate)
+library(PerformanceAnalytics)
+library(quantmod)
+
+getSymbols("^GSPC")
 
 shinyServer(
   function(input, output) {
+    #FromDate <- today()
+    #year(FromDate) <- year(FromDate) - 1
+    #output$Temp <- as.character(FromDate)
 
+    Symbol <- reactive({
+      input$LookupButton
+      isolate(toupper(input$Symbol))
+    })
+
+    FullResults <- reactive({
+      if (Symbol() != "") {
+        getSymbols(Symbol())
+        Intermediate <- get(Symbol())
+        adjustOHLC(Intermediate, symbol.name=Symbol())
+      }
+    })
+
+    Prices <- reactive({
+      FullResults()[paste(input$FromDate, "/")]
+    })
+
+    GSPCPrices <- reactive({
+      GSPC[paste(input$FromDate, "/")]
+    })
+
+    Returns <- reactive({
+      dailyReturn(Prices())
+    })
+
+    GSPCReturns <- reactive({
+      dailyReturn(GSPCPrices())
+    })
+
+    MonthlyReturns <- reactive({
+      monthlyReturn(Prices())
+    })
+
+    MonthlyGSPCReturns <- reactive({
+      monthlyReturn(GSPCPrices())
+    })
+
+    PairedReturns <- reactive({
+      Intermediate <- cbind(Returns(), GSPCReturns())
+      colnames(Intermediate) <- c(Symbol(), "S&P 500")
+      Intermediate
+    })
+
+    PairedMonthlyReturns <- reactive({
+      Intermediate <- cbind(MonthlyReturns(), MonthlyGSPCReturns())
+      colnames(Intermediate) <- c(Symbol(), "S&P 500")
+      Intermediate
+    })
+
+    output$Chart <- renderPlot({
+      if (Symbol() == "")
+        NULL
+      else {
+        #chartSeries(Prices(), log.scale = T, type="bars", name=Symbol(), minor.ticks = F)
+        charts.PerformanceSummary(PairedReturns(), colorset=rich6equal[-1], lwd=2, ylog=TRUE)
+      }
+    },
+    res=144)
+
+    output$MonthlyReturns <- renderTable({
+      if (Symbol() == "")
+        NULL
+      else
+        table.CalendarReturns(PairedMonthlyReturns())
+    })
+
+    output$MonthlyStats <- renderTable({
+      if (Symbol() == "")
+        NULL
+      else
+        table.Stats(PairedMonthlyReturns())
+    })
   }
 )
--- ui.R
+++ ui.R
@@ -1,13 +1,29 @@
 library(shiny)
+library(lubridate)
 
 shinyUI(
-  pageWithSidebar(
-    headerPanel("Portfolio Analyser"),
-    sidebarPanel(
-      h3("Enter Stock Symbol")
-    ),
-    mainPanel(
-      h3("Main Panel")
+  fluidPage(title="Analyse Stock", theme = "cerulean.css",
+    titlePanel("Analyse Stock", "Analyse Stock"),
+    fixedPanel(width=1080, left=20, right=20, p("This App uses the quantmod and PortfolioAnalytics packages to do analysis of a stock's price history.  Enter a stock symbol (such as YHOO, AAPL or MSFT) and press the Lookup button (twice initially due to an issue with shinyapps.io). This will download the stocks history and produce the analysis.  Once the symbol has been enter the start date can be modified and the analysis will be updated reactively.")),
+    sidebarLayout(
+      fixedPanel(width=260, left=20, top=220,
+        fluidRow(
+          column(tags$b("Symbol:"), width=4),
+          column(textInput("Symbol", NULL), width=4),
+          column(actionButton("LookupButton", "Lookup", class="btn btn-primary"), width=4)
+        ),
+        fluidRow(
+          column(tags$b("Date:"), width=4),
+          column(dateInput("FromDate", NULL, format = "dd-M-yyyy", value = Sys.Date()-365), width=8)
+        ),
+        tableOutput("MonthlyStats")
+      ),
+      fixedPanel(left=320, top=200,
+        plotOutput("Chart", height=600, width=800),
+        tableOutput("MonthlyReturns"),
+        textOutput("Message")
+      ),
+      fluid = F
     )
   )
 )
